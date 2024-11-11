103556284612f794374377f3f74ce091f6c1061e
MiTsErRobot1943 <bna567@outlook.com>
2024-11-11 17:25:06 +0000

    Half way on setting up navigation KEEP WORKING !!!!!

--- app.py
+++ app.py
@@ -2,12 +2,11 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
-@app.route('../templates/index.html')
-def hello_world():  # put application's code here
+# Define a proper route using URL notation
+@app.route('/')
+def hello_world(name=None):
     user = {'name': 'Blair'}
     return render_template('index.html', user=user)
 
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000 , debug=True)
+    app.run(host='0.0.0.0', debug=True)

94eae9b317e90ce0bef79dc6a38915de6e6a9abf
MiTsErRobot1943 <bna567@outlook.com>
2024-11-25 16:39:06 +0000

    Me the idiot has finally commited

--- app.py
+++ app.py
@@ -2,11 +2,29 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# Define a proper route using URL notation
+# Define the root route
 @app.route('/')
-def hello_world(name=None):
+def index():
     user = {'name': 'Blair'}
     return render_template('index.html', user=user)
+
+# Define route for Genre
+@app.route('/genre')
+def genre():
+    user = {'name': 'Blair'}
+    return render_template('Genre.html', user=user)
+
+# Define route for Contact
+@app.route('/contact')
+def contact():
+    user = {'name': 'Blair'}
+    return render_template('Contact.html', user=user)
+
+# Define route for Login/Sign-in
+@app.route('/login')
+def login():
+    user = {'name': 'Blair'}
+    return render_template('Login_Sign-in.html', user=user)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)

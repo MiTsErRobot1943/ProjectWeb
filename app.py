89cc1c04b5a49c29a66ef806230b036bd77cab48
MiTsErRobot1943 <bna567@outlook.com>
2024-11-26 19:46:24 +0000

    Got nav working :)

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 # Define the root route
 @app.route('/')
-def index():
+def home():
     user = {'name': 'Blair'}
     return render_template('index.html', user=user)
 
